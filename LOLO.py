e5b31e59963ab724222ea620aa51bb55508d9230
EmmerlingM <grabarz444@o2.pl>
2021-01-01 22:56:35 +0100

    Initial linear model

--- LOLO.py
+++ LOLO.py
@@ -1,6 +1,17 @@
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+import sklearn.linear_model as sk
 table = pd.read_csv("loltable.csv")
 table = table.iloc[:,6:]
-np.histogram(table[:,3])
+table.iloc[:,3].hist()
+nptable = np.array(table)
+
+fig, ax = plt.subplots()
+ax.hist(nptable[:,3], bins=200)
+plt.xlabel("Gold")
+plt.ylabel("Frequency of ocurrence")
+ax.set_axisbelow(True)
+ax.grid(b=True, which='both', axis='both', color='black', linewidth=0.7, alpha=0.2)
+
+regressor = sk.LinearRegression()
